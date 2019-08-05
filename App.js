2a96424ae44ea5aab3476b6224403569cb29b7bc
iAmir <amirabgta@gmail.com>
2019-08-05 17:39:20 +0430

    add pages to navigation config

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createDrawerNavigator, createAppContainer } from "react-navigation";
+import { heightPercentageToDP, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import ListScreen from './src/pages/main/list.js';
+import ServerScreen from './src/pages/server/server.js';
+
+const noHeaderNavigationOptions = {
+  header: null,
+  headerMode: 'none'
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppStack = createStackNavigator(
+  {
+    HomePage: { screen: ListScreen, navigationOptions: noHeaderNavigationOptions },
+    ServerPage: { screen: ServerScreen, navigationOptions: noHeaderNavigationOptions },
   },
-});
+  {
+    initialRouteName: 'HomePage',
+  }
+);
+
+const AppContainer = createAppContainer(AppStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
